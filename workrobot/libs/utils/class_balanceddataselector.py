31054ae94080c13f85a4561845d76efe93a689c9
plutoese <glen.zhang7@gmail.com>
2016-08-12 14:05:40 +0800

    select done

--- workrobot/libs/utils/class_balanceddataselector.py
+++ workrobot/libs/utils/class_balanceddataselector.py
@@ -52,38 +52,12 @@ class BalancedDataSelector:
 
         self._nrows, self._ncols = self._data.shape
 
-    def select(self, max_na_number_per_variable = None):
-        # 设置基础数据
-        _data = self.data_with_na.loc[:,lambda df: df.sum() <= max_na_number_per_variable]
-
-        # 重新设置行变量和列变量
-        # 设置行变量
-        indexes = range(_data.shape[0])
-        indexes_mapping = dict(zip(indexes, _data.index))
-        _data.index = indexes
-
-        # 设置变量
-        columns = [str(i) for i in range(_data.shape[1])]
-        columns_mapping = dict(zip(columns, _data.columns))
-        _data.columns = columns
-
-        #print(_data)
-        self.test_data = _data
-
-        # 单个变量的索引表（变量：单个变量缺失值的位置）
-        print(_data.columns)
-        single_variable_false_position_dict = {frozenset([col]):set(_data[col][_data[col]].index)
-                                               for col in _data.columns}
-        for key in single_variable_false_position_dict:
-            print('LETTTTTTTTTTTTTTTTTT')
-            print(key,set(key))
-            print(columns_mapping[set(key).pop()], [indexes_mapping[cc] for cc in single_variable_false_position_dict[key]])
-
-        print(single_variable_false_position_dict)
+    @classmethod
+    def assembly(cls, single_variable_false_position_dict=None, max_na_number_per_variable=None):
         selected_dataframe = dict()
         selected_dataframe[1] = single_variable_false_position_dict
 
-        for round in range(2, self._ncols):
+        for round in range(2, len(selected_dataframe[1])):
         #for round in range(2, 20):
             variable_dict_each_round_kept = selected_dataframe[round-1]
 
@@ -102,13 +76,70 @@ class BalancedDataSelector:
             print('round here', round)
             selected_dataframe[round] = union_key_dict_deleted
 
-        _dataframe = selected_dataframe
+        return selected_dataframe
+
+    def select(self, max_na_number_per_variable = None):
+        # 设置基础数据
+        _data = self.data_with_na.loc[:,lambda df: df.sum() <= max_na_number_per_variable]
+
+        # 重新设置行变量和列变量
+        # 设置行变量
+        indexes = range(_data.shape[0])
+        indexes_mapping = dict(zip(indexes, _data.index))
+        _data.index = indexes
+        '''
+        # 设置变量
+        columns = [str(i) for i in range(_data.shape[1])]
+        columns_mapping = dict(zip(columns, _data.columns))
+        _data.columns = columns
+        '''
+        column_index = {col:frozenset(_data[col][_data[col]].index) for col in _data.columns}
+        column_index_reverse = [(column_index[col],col) for col in column_index]
+        print(len(column_index))
+        print(column_index_reverse)
+
+        index_columns_dict = defaultdict(set)
+        for k, v in column_index_reverse:
+            index_columns_dict[k].add(v)
+        print(index_columns_dict.items())
+        print(len(index_columns_dict))
+
+        col_variables_dict = {list(index_columns_dict[key])[0]:index_columns_dict[key] for key in index_columns_dict}
+        print(col_variables_dict)
+
+        _data = _data.loc[:, col_variables_dict.keys()]
+        columns = [str(i) for i in range(_data.shape[1])]
+        columns_mapping = dict(zip(columns, _data.columns))
+        columns_mapping = {key:col_variables_dict[columns_mapping[key]] for key in columns_mapping}
+        _data.columns = columns
+
+        print(len(_data.columns))
+        print(_data.columns)
+        print(columns_mapping)
+        self.test_data = _data
+
+
+        # 单个变量的索引表（变量：单个变量缺失值的位置）
+        print(_data.columns)
+        single_variable_false_position_dict = {frozenset([col]):set(_data[col][_data[col]].index)
+                                               for col in _data.columns}
+        for key in single_variable_false_position_dict:
+            print('LETTTTTTTTTTTTTTTTTT')
+            print(key,set(key))
+            print(columns_mapping[set(key).pop()], [indexes_mapping[cc] for cc in single_variable_false_position_dict[key]])
+
+        print(single_variable_false_position_dict)
+
+        selected_dataframe = BalancedDataSelector.assembly(single_variable_false_position_dict, max_na_number_per_variable)
 
         _dataframe = dict()
         for i in selected_dataframe:
             _dataframe[i] = dict()
             for j in selected_dataframe[i]:
-                _key = frozenset([columns_mapping[key] for key in j])
+                _key = []
+                for key in j:
+                    _key.extend(columns_mapping[key])
+                _key = frozenset(_key)
                 _dataframe[i][_key] = [indexes_mapping[key] for key in selected_dataframe[i][j]]
         print(_dataframe)
         return _dataframe
@@ -133,7 +164,7 @@ if __name__ == '__main__':
     F = open(r'E:\github\workrobot\workrobot\files\ceic_raw_data_year.pkl', 'rb')
     raw_data = pickle.load(F)
     df = raw_data.loc[2010]
-    df = df.iloc[0:500,0:30]
+    #df = df.iloc[0:500,0:80]
     #df = df.loc[:, ['互联网宽帶接入用户','从业人数_制造业','人口数','从业人员']]
     df_bool = df.isnull()
     #print(df_bool)
@@ -144,16 +175,16 @@ if __name__ == '__main__':
     # 调用函数
     max_na_number_per_variable = 5
     result = bselector.select(max_na_number_per_variable=max_na_number_per_variable)
-    '''
+
     # 输出结果打印
     print('\n\n=======================result=====================')
     for i in sorted(result):
         print('Variable Numbers: {}'.format(i))
         for key in result[i]:
             print(key,' --> ',result[i][key])
-        print('\n')'''
+        print('\n')
 
-    '''
+
     #df = bselector.test_data
     # 检验
     CHECK_TIMES = 1000
@@ -174,5 +205,5 @@ if __name__ == '__main__':
             print(len(mdata.index),len(mdata.dropna().index),len(mdata.index)-len(mdata.dropna().index))
             print(mdata[mdata.isnull().any(axis=1)])
             print('WRONG!!!!!!')
-            break'''
+            break
 
